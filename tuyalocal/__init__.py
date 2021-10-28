44645a4c8c1257223d44b25bc89b26286ff4a287
alexanv1 <44785744+alexanv1@users.noreply.github.com>
2021-10-27 23:32:24 -0700

    Delete TuyaLocal since there are better local control Tuya implementations now and I switched to official integration

--- tuyalocal/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Simple platform to control **SOME** Tuya switch devices.
-
-For more details about this platform, please refer to the documentation at
-https://home-assistant.io/components/switch.tuya/
-"""
-import voluptuous as vol
-DOMAIN = 'tuyalocal'
